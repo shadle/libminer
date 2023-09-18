9dea80ab5aa99c95682bea61d13165279abe335a
John Shadle <john.shadle@gmail.com>
2023-09-18 16:40:22 -0500

    Change function to use helper function calculate_sizes

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -23,13 +23,15 @@ lib_summary <- function(sizes = FALSE) {
   names(pkg_df) <- c("Library", "n_packages")
 
   if (sizes) {
-    pkg_df$lib_size <- vapply(
-      pkg_df$Library,
-      function(x) {
-        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
-      },
-      FUN.VALUE = numeric(1)
-    )
+    pkg_df <- calculate_sizes(pkg_df)
   }
   pkg_df
+}
+
+calculate_sizes <- function(df) {
+  df$lib_size <- map_dbl(
+    df$Library,
+    ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
+    )
+  df
 }
